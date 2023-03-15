45e72d04f485bd9bc26fb0a2d8a6c59a4ec3b0b9
Bruce Liu <jianey.liu@oracle.com>
2023-03-15 18:39:38 -0400

    add QR image size

--- cmd/qrcode.go
+++ cmd/qrcode.go
@@ -9,6 +9,7 @@ import (
 )
 
 var rawText, outPath string
+var size int
 var qrcodeCmd = &cobra.Command{
 	Use:   "qrcode command",
 	Short: "qr code generator",
@@ -19,11 +20,12 @@ var qrcodeCmd = &cobra.Command{
 func init() {
 	qrcodeCmd.Flags().StringVarP(&rawText, "text", "t", "", "text to be encoded")
 	qrcodeCmd.Flags().StringVarP(&outPath, "out", "o", "qr.png", "file name of the qrfile")
+	qrcodeCmd.Flags().IntVarP(&size, "size", "s", 256, "QR image size")
 	qrcodeCmd.MarkFlagRequired("text")
 }
 
 func qrcodeRun(cmd *cobra.Command, args []string) {
-	err := qrcode.WriteFile(rawText, qrcode.Medium, 256, outPath)
-	fmt.Fprintf(os.Stdout, "qrcode file generated: %s", rawText)
+	err := qrcode.WriteFile(rawText, qrcode.Medium, size, outPath)
 	cobra.CheckErr(err)
+	fmt.Fprintf(os.Stdout, "qrcode file generated: %s", rawText)
 }
